c18ffd181fe22d71832337feba5d43f43b7d476b
Fakhry <fakhry.linux@gmail.com>
2023-11-01 14:15:46 +0800

    Implementasi Modul di Vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <p class="animate__animated animate__fadeInRight">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto illum
+      modi optio placeat velit. Ad animi deleniti enim error fuga, maxime natus
+      nemo obcaecati odit, placeat reiciendis repellat tenetur voluptatum!
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'animate.css/animate.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faShoppingCart);
 
@@ -12,4 +12,4 @@ Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
