a4a74c60acaacfbfc2a34d4241e85a23905faa20
Fakhry <fakhry.linux@gmail.com>
2023-12-26 03:56:09 +0800

    Membuat Component ProductList

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>IDShop</h1>
-    <p class="animate__animated animate__fadeInRight">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto illum
-      modi optio placeat velit. Ad animi deleniti enim error fuga, maxime natus
-      nemo obcaecati odit, placeat reiciendis repellat tenetur voluptatum!
-    </p>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-    <price :value="4.23"></price>
+    <product-list :products="products" :maximum="maximum"></product-list>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import Price from "@/components/PriceComponent.vue";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import ProductList from "@/components/ProductList.vue";
 
 export default {
   name: "App",
+  data: function () {
+    return {
+      maximum: 20,
+      products: [],
+    };
+  },
   components: {
-    FontAwesomeIcon,
-    Price,
+    ProductList,
+    // FontAwesomeIcon,
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
   },
 };
 </script>
